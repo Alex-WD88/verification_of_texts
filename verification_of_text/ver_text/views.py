2b05e760ace60b71e37ebce8a793faf473514500
Alex-WD88 <dobryanskiy.aleksandr@gmail.com>
2023-07-10 18:51:59 +0300

    begin 10.07.23

--- verification_of_text/ver_text/views.py
+++ verification_of_text/ver_text/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'ver_text/index.html')
+
+
+def about(request):
+    return HttpResponse('<h4>about</h4>')
